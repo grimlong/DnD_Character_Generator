01dcef47995d786b0a86a401ba319c257b4b0508
Grimlong <jasonmillers197@gmail.com>
2021-02-10 22:07:15 -0500

    EOD 2/10

--- js/app.js
+++ js/app.js
@@ -6,38 +6,28 @@ const apiData = {
 const {url,_class} = apiData
 const apiClass = `${url}${_class}`
 
-
 fetch(apiClass)
   .then( (data) => data.json() )
   .then( (classes) => _classList(classes))
 
-
 let _classList = (data) => {
 
 const dtr = data.results
-
-
-  let cList = ["Classes", dtr[0].name, dtr[1].name, dtr[2].name, dtr[3].name,dtr[4].name, dtr[5].name, dtr[6].name, dtr[7].name, dtr[8].name, 
-               dtr[9].name, dtr[10].name, dtr[11].name]
   
-  // let cList =[];
-  // for (i = 0; i < data.results.length; i++){
-  //     cList.push.apply(cList, data.results[i].name);
-  // }
-
-  var sel = document.getElementById('classList');
-  var fragment = document.createDocumentFragment();
+let cList =["classes",...dtr.map(x => x.name)];
+   
+var sel = document.getElementById('classList');
+var fragment = document.createDocumentFragment();
   
-  cList.forEach(function(cList, index) {
-      var opt = document.createElement('option');
-      opt.innerHTML = cList;
-      opt.value = cList;
-      fragment.appendChild(opt);
+cList.forEach(function(cList, index) {
+  var opt = document.createElement('option');
+  opt.innerHTML = cList;
+  opt.value = cList;
+  fragment.appendChild(opt);
   });
   
   sel.appendChild(fragment);
 }
-
 
 fetch(apiClass + '/barbarian')
  .then( (data0) => data0.json())
